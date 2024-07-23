2055fdb5068c1b71f7d21d34a7de9893e5b3c8b2
kubrakaradirek <karadirekkubra96@gmail.com>
2024-07-23 11:23:05 +0300

    The Customer folder has been defined, and the Web API project has been created. According to N-Tier architecture, the file names within the project have been organized, and the entities have been written with their properties.

---
+++ Services/Customer/TesodevBackendC.Customer.WebApi/Entities/Address.cs
@@ -0,0 +1,15 @@
+﻿namespace TesodevBackendC.Customer.WebApi.Entities
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public string AddressLine { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public int CityCode { get; set; }
+        public Guid CustomerId { get; set; }
+        public Customer Customer { get; set; }
+        public Guid OrderDetailId { get; set; }
+
+    }
+}
---
+++ Services/Customer/TesodevBackendC.Customer.WebApi/Entities/Customer.cs
@@ -0,0 +1,14 @@
+﻿namespace TesodevBackendC.Customer.WebApi.Entities
+{
+    public class Customer
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public List<Address> Address { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public Guid OrderDetailId { get; set; }
+
+    }
+}
